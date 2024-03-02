e5e56455e638d9f77e4e98ba66e3367dceaad5bd
Steven John <149338927+zzsjnvts@users.noreply.github.com>
2024-03-03 05:03:22 +0800

    Update efi.py

--- efi.py
+++ efi.py
@@ -1,5 +1,25 @@
 import random
+import os
+import sys
+import time
+print("EFI by Aesthetic")
+print(" ")
+print(" ")
 
+def animated_loading(seconds):
+    start_time = time.time()
+    animation = "|/-\\"
+    idx = 0
+
+    while time.time() - start_time < seconds:
+        sys.stdout.write("\r" + "Registering EFI " + animation[idx % len(animation)])
+        sys.stdout.flush()
+        time.sleep(0.1)
+        idx += 1
+    print(" ")
+    print(" ")
+    sys.stdout.write("\r" + "EFI has been successfully registered in the system!    \n")
+    
 def randomize_15_chars(input_str):
     space_index = input_str.find(" ", input_str.find("/")+3)
     
@@ -23,7 +43,18 @@ def process_file(input_file_path, output_file_path):
         output_file.writelines(content)
 
 input_file_path = "Startup.nsh"
-output_file_path = "output.nsh"
+output_file_path = "Startup.nsh"
 process_file(input_file_path, output_file_path)
+directory_path = 'efi\\boot'
+file_path = os.path.join(directory_path, 'startup.nsh')
+if os.path.exists(directory_path) and os.path.isdir(directory_path):
+    output_file_path = "efi\\boot\\startup.nsh"
+    process_file(input_file_path, output_file_path)
+    
+loading_time = 5  
+animated_loading(loading_time)
+input("Press enter to exit..")
+    
+
 
 
